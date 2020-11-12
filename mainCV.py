7282cf75c2e47c4a72cebd187b6303997a41ec94
ALat <ahykumin@gmail.com>
2020-11-12 13:12:23 -0500

    edits

--- mainCV.py
+++ mainCV.py
@@ -11,52 +11,12 @@ if not (cap.isOpened()):
 	print("Could not open video device")
 
 #open usb port
-usb_interface.usb_connection(port,1)
+link = txfer.SerialTransfer(port, baud=115200)
+usb_interface.usb_connection(link,1)
 
 #To set the resolution
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,640);
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT,480);
-
-#start circle detection until circl is returned
-def circle_bound():
-# Capture frame-by-frame
-	ret, frame = cap.read()
-	overlay = frame.copy();
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	bound_obtained = False
-	while(not bound_obtained):
-		# detect circles in the frame
-		circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 100)
-		# ensure at least some circles were found
-		if circles is not None:
-			# convert the (x, y) coordinates and radius of the circles to integers
-			circles = np.round(circles[0, :]).astype("int")
-			# loop over the (x, y) coordinates and radius of the circles
-			for (x, y, r) in circles:
-				# draw the circle in the output image, then draw a rectangle
-				# corresponding to the center of the circle
-				cv2.circle(overlay, (x, y), r, (0, 255, 0), 4)
-				# show the output image
-				cv2.imshow("output", overlay)
-			bound_obtained = True
-		else:
-			#print("nothing detected")
-			pass
-		cv2.imshow("output", overlay)
-
-def update_frame():
-	cv2.imshow("output", overlay)
-
-
-if __name__ == '__main__':
-	
-
-	circle_bound()
-	cv2.imshow("output", overlay)
-	update_frame()
-	exit_check()
-
-
 
 def exit_check():
 	if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -64,3 +24,40 @@ def exit_check():
 		cap.release()
 		cv2.destroyAllWindows()
 		usb_interface.usb_connection(port,0)
+#start circle detection until circl is returned
+def circle_bound():
+# Capture frame-by-frame
+	pass
+
+
+
+if __name__ == '__main__':
+	bound_obtained = False
+
+	while(True):
+		ret, frame = cap.read()
+		overlay = frame.copy();
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		while(not bound_obtained):
+			# detect circles in the frame
+			circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 100)
+			# ensure at least some circles were found
+			if circles is not None:
+				# convert the (x, y) coordinates and radius of the circles to integers
+				circles = np.round(circles[0, :]).astype("int")
+				# loop over the (x, y) coordinates and radius of the circles
+				for (x, y, r) in circles:
+					# draw the circle in the output image, then draw a rectangle
+					# corresponding to the center of the circle
+					cv2.circle(overlay, (x, y), r, (0, 255, 0), 4)
+					# show the output image
+					cv2.imshow("output", overlay)
+				bound_obtained = True
+			else:
+				#print("nothing detected")
+				pass
+		cv2.imshow("output", overlay)
+		exit_check()
+
+
+
--- usb_interface.py
+++ usb_interface.py
@@ -2,16 +2,14 @@ print("usb interface")
 from pySerialTransfer import pySerialTransfer as txfer
 import time
 
-def usb_connection(port,status):
+def usb_connection(link,status):
     if status==1:
-        link = txfer.SerialTransfer(port, baud=115200)
-
         link.open()
         time.sleep(2) # allow some time for the Arduino to completely reset
     else:
     	link.close()
 
-def usb_send(port,pos_list):
+def usb_send(link, pos_list):
     for a in range(6):
         link.txBuff[a] = pos_list[a]
 
