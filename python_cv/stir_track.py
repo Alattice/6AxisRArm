5cc7042813d5cab67715bb24865a73e3b6584521
ALat <ahykumin@gmail.com>
2023-09-14 11:33:44 -0400

    wip window thread

--- python_cv/stir_track.py
+++ python_cv/stir_track.py
@@ -21,25 +21,29 @@ import queue
 import traceback
 import sys
 
-import usb_linker as usb
+from usb_linker import usb_linker as usb
 #import window_process as view
 
 class window_process(threading.Thread):
-	def __init__(self, frame_data):
-		threading.Thread.__init__(self)
+	def __init__(self,ready,vid_dev):
 		self.window_W = 640
 		self.window_H = 480
-		self.cam_stream = None
+		self.cam_stream = cv2.VideoCapture(vid_dev)
 		self.overlay = None
-		self.init()
+		threading.Thread.__init__(self)
+		self.start(vid_dev)
+		time.sleep(1)
 		self.window_proc = threading.Thread(target=self.update)
 		self.window_proc.name = 'window_update_manager'
 		self.window_proc.start()
+		print("thread started")
+		ready = True
 
-	def init(self):
+
+	def start(self, vid_cap_device):
 		try:
 			# Open the device at the ID 0
-			self.cam_stream = cv2.VideoCapture(self.vid_cap_device)
+			#self.cam_stream = cv2.VideoCapture(vid_cap_device)
 			
 
 			#Check whether user selected camera is opened successfully.
@@ -55,7 +59,13 @@ class window_process(threading.Thread):
 			print("error relating to video device occured: {}".format())
 
 	def update(self): #update the window
-		cv2.imshow("output", overlay)
+		ret, frame = self.cam_stream.read()
+		if frame is not None:
+			self.overlay = frame.copy();
+			time.sleep(0.05)
+			cv2.imshow("output", self.overlay)
+		else:
+			time.sleep(1)
 		pass
 
 	def close(self):
@@ -117,9 +127,10 @@ class cv_module(threading.Thread):
 	# 	pass
 
 #============================== main routine ================================
+ready = False
 robot_arm = usb()
 session = cv_module()
-cam_view = view()
+cam_view = window_process(ready,session.vid_cap_device)
 
 #set default robot joint pos
 for a in range(6):
@@ -132,83 +143,84 @@ cvxyr = [0,0,0]
 angle = 100
 
 while (True): #main routine
-	ret, frame = session.cam_stream.read()
-	cam_view.overlay = frame.copy();
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	if not circle_gen:
-		circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 100)
+	if(ready):
+		ret, frame = cam_view.cam_stream.read()
+		#cam_view.overlay = frame.copy();
+		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		if not circle_gen:
+			circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 100)
 
-		if circles is not None:
-		# convert the (x, y) coordinates and radius of the circles to integers
-			circles = np.round(circles[0, :]).astype("int")
-		# loop over the (x, y) coordinates and radius of the circles
-			for (x, y, r) in circles:
-			# draw the circle in the output image, then draw a rectangle
-			# corresponding to the center of the circle
-				cv2.circle(overlay, (x, y), r, (0, 255, 0), 4)
-				if (r > 0) and (x > 0) and (y > 0):
-					xyr[0] = int(100-100*x/session.window_W)
-					xyr[1] = int(100-100*y/session.window_H)
-					xyr[2] = int(100*r/480)
-					cvxyr = x,y,r
-					origin = [xyr[0],xyr[1]]
-					increment = 40
-					rad = xyr[2]
-					circle_gen = True
-			
-	#stir if circlei
-	if circle_gen:
-		try:
-				ret, frame = session.cam_stream.read()
-				cam_view.overlay = frame.copy();
-				#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-				#screen_stat = str("x: {} y: {} r: {}".format(xyr[0],xyr[1],xyr[2]))
-				#cv2.putText(cam_view.overlay,screen_stat,(5,15),cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2,cv2.LINE_AA)
-				cv2.circle(cam_view.overlay, (cvxyr[0], cvxyr[1]), cvxyr[2], (0, 255, 0), 4)
-				#cv2.imshow("output", overlay)
+			if circles is not None:
+			# convert the (x, y) coordinates and radius of the circles to integers
+				circles = np.round(circles[0, :]).astype("int")
+			# loop over the (x, y) coordinates and radius of the circles
+				for (x, y, r) in circles:
+				# draw the circle in the output image, then draw a rectangle
+				# corresponding to the center of the circle
+					cv2.circle(cam_view.overlay, (x, y), r, (0, 255, 0), 4)
+					if (r > 0) and (x > 0) and (y > 0):
+						xyr[0] = int(100-100*x/cam_view.window_W)
+						xyr[1] = int(100-100*y/cam_view.window_H)
+						xyr[2] = int(100*r/480)
+						cvxyr = x,y,r
+						origin = [xyr[0],xyr[1]]
+						increment = 40
+						rad = xyr[2]
+						circle_gen = True
+				
+		#stir if circlei
+		if circle_gen:
+			try:
+					ret, frame = cam_view.cam_stream.read()
+					cam_view.overlay = frame.copy();
+					#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+					#screen_stat = str("x: {} y: {} r: {}".format(xyr[0],xyr[1],xyr[2]))
+					#cv2.putText(cam_view.overlay,screen_stat,(5,15),cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2,cv2.LINE_AA)
+					cv2.circle(cam_view.overlay, (cvxyr[0], cvxyr[1]), cvxyr[2], (0, 255, 0), 4)
+					#cv2.imshow("output", overlay)
 
-				j1 = origin[0]+rad*math.cos(math.radians(angle))*math.exp(angle/(-360))
-				j2 = origin[1]+rad*math.sin(math.radians(angle))+(angle/360)*3
-				j5 = 54-7*math.sin(math.radians(angle))*math.exp(angle/(-360))
-
-
-				robot_arm.usb_link.txBuff[0] = math.floor(j1)
-				robot_arm.usb_link.txBuff[1] = math.floor(j2)
-				robot_arm.usb_link.txBuff[4] = math.floor(j5)
-
-				print(robot_arm.usb_link.txBuff[0],robot_arm.usb_link.txBuff[1],robot_arm.usb_link.txBuff[4])
+					j1 = origin[0]+rad*math.cos(math.radians(angle))*math.exp(angle/(-360))
+					j2 = origin[1]+rad*math.sin(math.radians(angle))+(angle/360)*3
+					j5 = 54-7*math.sin(math.radians(angle))*math.exp(angle/(-360))
 
 
-				robot_arm.usb_link.send(6)
+					robot_arm.usb_link.txBuff[0] = math.floor(j1)
+					robot_arm.usb_link.txBuff[1] = math.floor(j2)
+					robot_arm.usb_link.txBuff[4] = math.floor(j5)
 
-				angle += increment
-				if(angle >= 360):
-					angle = 0
-					circle_gen = False
-
-				while not robot_arm.usb_link.available():
-					if robot_arm.usb_link.status < 0:
-						print('ERROR: {}'.format(robot_arm.usb_link.status))
-
-				response = [0,0,0,0,0,0]
-
-				for index in range(robot_arm.usb_link.bytesRead):
-					response[index] = int(robot_arm.usb_link.rxBuff[index])
+					print(robot_arm.usb_link.txBuff[0],robot_arm.usb_link.txBuff[1],robot_arm.usb_link.txBuff[4])
 
 
-				print(response)
-				#time.sleep(rad*0.01)
-		except KeyboardInterrupt or (cv2.waitKey(1) & 0xFF == ord('q')):
+					robot_arm.usb_link.send(6)
+
+					angle += increment
+					if(angle >= 360):
+						angle = 0
+						circle_gen = False
+
+					while not robot_arm.usb_link.available():
+						if robot_arm.usb_link.status < 0:
+							print('ERROR: {}'.format(robot_arm.usb_link.status))
+
+					response = [0,0,0,0,0,0]
+
+					for index in range(robot_arm.usb_link.bytesRead):
+						response[index] = int(robot_arm.usb_link.rxBuff[index])
+
+
+					print(response)
+					#time.sleep(rad*0.01)
+			except KeyboardInterrupt or (cv2.waitKey(1) & 0xFF == ord('q')):
+				robot_arm.close()
+				cam_view.cam_stream.release()
+				cv2.destroyAllWindows()
+
+		screen_stat = str("x: {} y: {} r: {}".format(xyr[0],xyr[1],xyr[2]))
+		cv2.putText(cam_view.overlay,screen_stat,(5,15),cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0,0,255),2,cv2.LINE_AA)
+		#cv2.imshow("output", overlay)
+		if cv2.waitKey(1) & 0xFF == ord('q'):
 			robot_arm.close()
-			session.cam_stream.release()
+			cam_view.cam_stream.release()
 			cv2.destroyAllWindows()
-
-	screen_stat = str("x: {} y: {} r: {}".format(xyr[0],xyr[1],xyr[2]))
-	cv2.putText(cam_view.overlay,screen_stat,(5,15),cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0,0,255),2,cv2.LINE_AA)
-	#cv2.imshow("output", overlay)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		robot_arm.close()
-		session.cam_stream.release()
-		cv2.destroyAllWindows()
-		cam_view.close()
-		break
+			cam_view.close()
+			break
