ca6b8c3dc800bd7ed8a47abcb105bee33c687aef
Chen <chenming@siemens.com>
2023-09-14 10:48:08 -0400

    window process

--- python_cv/stir_track.py
+++ python_cv/stir_track.py
@@ -22,37 +22,21 @@ import traceback
 import sys
 
 import usb_linker as usb
+import window_process as view
 
-
-class cv_module(threading.Thread):
-	def __init__(self):
+class window_process(threading.Thread):
+	def __init__(self, frame_data):
 		threading.Thread.__init__(self)
-		
-		#cv
-		self.vid_cap_device = 0 #default webcam on linux
-		#self.videoStream
 		self.window_W = 640
 		self.window_H = 480
 		self.cam_stream = None
+		self.overlay = None
+		self.init()
+		self.window_proc = threading.Thread(target=self.update)
+		self.window_proc.name = 'window_update_manager'
+		self.window_proc.start()
 
-		self.user_params()
-		self.init_window()
-
-		#start window thread
-		#self.window = window_process.window_handler()
-			
-
-	def user_params(self): #cmd args from user
-		num_args = len(sys.argv)
-		if num_args == 1: #no param given, assume video0
-			#self.vid_cap_device = 0
-			print("No user input, assuming {}".format(self.vid_cap_device))
-		else:
-			self.vid_cap_device = int(sys.argv[1])
-			print("Using {} as video input".format(self.vid_cap_device))
-
-
-	def init_window(self):
+	def init(self):
 		try:
 			# Open the device at the ID 0
 			self.cam_stream = cv2.VideoCapture(self.vid_cap_device)
@@ -70,12 +54,72 @@ class cv_module(threading.Thread):
 		except Exception as err:
 			print("error relating to video device occured: {}".format())
 
-	def window_handler(self):
+	def update(self): #update the window
+		cv2.imshow("output", overlay)
 		pass
+
+	def close(self):
+		self.window_proc.join()
+		pass
+
+#------------------------------window end-----------------
+#------------------------------cv other start------------
+
+class cv_module(threading.Thread):
+	def __init__(self):
+		threading.Thread.__init__(self)
+		
+		#cv
+		self.vid_cap_device = 0 #default webcam on linux
+		#self.videoStream
+		# self.window_W = 640
+		# self.window_H = 480
+		# self.cam_stream = None
+
+
+		self.user_params()
+		#self.init_window()
+		
+
+		#start window thread
+		#self.window = window_process.window_handler()
+			
+	# fetch user args from console. takes 1 video arg [-1/0/1]
+	def user_params(self): #cmd args from user
+		num_args = len(sys.argv)
+		if num_args == 1: #no param given, assume video0
+			#self.vid_cap_device = 0
+			print("No user input, assuming {}".format(self.vid_cap_device))
+		else:
+			self.vid_cap_device = int(sys.argv[1])
+			print("Using {} as video input".format(self.vid_cap_device))
+
+
+	# def init_window(self):
+	# 	try:
+	# 		# Open the device at the ID 0
+	# 		self.cam_stream = cv2.VideoCapture(self.vid_cap_device)
+			
+
+	# 		#Check whether user selected camera is opened successfully.
+	# 		if not (self.cam_stream.isOpened()):
+	# 			print("Could not open video device")
+	# 			return 1
+
+	# 		#set window resolution
+	# 		self.cam_stream.set(cv2.CAP_PROP_FRAME_WIDTH,self.window_W);
+	# 		self.cam_stream.set(cv2.CAP_PROP_FRAME_HEIGHT,self.window_H);
+
+	# 	except Exception as err:
+	# 		print("error relating to video device occured: {}".format())
+
+	# def window_handler(self):
+	# 	pass
 
 #============================== main routine ================================
 robot_arm = usb()
 session = cv_module()
+cam_view = view()
 
 #set default robot joint pos
 for a in range(6):
@@ -89,7 +133,7 @@ angle = 100
 
 while (True): #main routine
 	ret, frame = session.cam_stream.read()
-	overlay = frame.copy();
+	cam_view.overlay = frame.copy();
 	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 	if not circle_gen:
 		circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 100)
@@ -116,12 +160,12 @@ while (True): #main routine
 	if circle_gen:
 		try:
 				ret, frame = session.cam_stream.read()
-				overlay = frame.copy();
+				cam_view.overlay = frame.copy();
 				#gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 				#screen_stat = str("x: {} y: {} r: {}".format(xyr[0],xyr[1],xyr[2]))
-				#cv2.putText(overlay,screen_stat,(5,15),cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2,cv2.LINE_AA)
-				cv2.circle(overlay, (cvxyr[0], cvxyr[1]), cvxyr[2], (0, 255, 0), 4)
-				cv2.imshow("output", overlay)
+				#cv2.putText(cam_view.overlay,screen_stat,(5,15),cv2.FONT_HERSHEY_SIMPLEX, 0.5,(255,0,0),2,cv2.LINE_AA)
+				cv2.circle(cam_view.overlay, (cvxyr[0], cvxyr[1]), cvxyr[2], (0, 255, 0), 4)
+				#cv2.imshow("output", overlay)
 
 				j1 = origin[0]+rad*math.cos(math.radians(angle))*math.exp(angle/(-360))
 				j2 = origin[1]+rad*math.sin(math.radians(angle))+(angle/360)*3
@@ -160,10 +204,11 @@ while (True): #main routine
 			cv2.destroyAllWindows()
 
 	screen_stat = str("x: {} y: {} r: {}".format(xyr[0],xyr[1],xyr[2]))
-	cv2.putText(overlay,screen_stat,(5,15),cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0,0,255),2,cv2.LINE_AA)
-	cv2.imshow("output", overlay)
+	cv2.putText(cam_view.overlay,screen_stat,(5,15),cv2.FONT_HERSHEY_SIMPLEX, 0.5,(0,0,255),2,cv2.LINE_AA)
+	#cv2.imshow("output", overlay)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		robot_arm.close()
 		session.cam_stream.release()
 		cv2.destroyAllWindows()
+		cam_view.close()
 		break
